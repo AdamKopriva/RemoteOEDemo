f4df022c2bcbe5b2e908c887ad34eac2455ce4dd
Adam Kopriva <adam.kopriva@apprise.com>
2013-07-25 17:11:20 +0200

    test

--- RemoteOEDemo/js/main.js
+++ RemoteOEDemo/js/main.js
@@ -1,12 +1,5 @@
 var app = {
 
-    findByName: function () {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function (employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -15,19 +8,35 @@ var app = {
         }
     },
 
-    initialize: function() {
+    registerEvents: function() {
         var self = this;
-        this.store = new MemoryStore(function() {
-            self.renderHomeView();
-        });
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
     },
 
-    renderHomeView: function () {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    initialize: function () {
+        var self = this;
+        this.store = new MemoryStore(function () {
+            $('body').html(new HomeView(self.store).render().el);
+        });
+        this.registerEvents();
+    } 
 
 };
 
